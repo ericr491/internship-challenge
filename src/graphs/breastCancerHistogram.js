7cca517237462ff3283f2fb3dbbf51a76a825f80
Eric Ruan <eric.ruan.130@gmail.com>
2020-11-24 16:59:22 -0500

    first graph

--- src/graphs/breastCancerHistogram.js
+++ src/graphs/breastCancerHistogram.js
@@ -1,1 +1,54 @@
-const breastCancerPatientsArray = fetchBreastCancer()
+const buildPlot = async () => {
+  const breastCancerPatientsArray = await fetchBreastCancer()
+
+  const filtered = breastCancerPatientsArray.map(patient => patient.age_group)
+
+  const ages = [
+    '0 to 17',
+    '18 to 29',
+    '30 to 49',
+    '50 to 69',
+    '70 or Older',
+  ]
+
+  const count = [
+  ]
+
+  ages.forEach(age_group => {
+    count.push(
+      filtered.reduce((count, age) => {
+        if (age === age_group)
+          return count + 1
+        return count
+      }, 0)
+    )
+  })
+
+  console.log(count)
+
+  const data = [
+    {
+      histfunc: 'sum',
+      x: ages,
+      y: count,
+      type: 'histogram',
+      name: 'num of patients',
+    }
+  ]
+
+  const layout = {
+    title: 'Frequency of Breast Cancer among age groups',
+    xaxis: {
+      title: 'Age Groups',
+    },
+    yaxis: {
+      title: 'Frequency',
+    },
+  }
+
+  Plotly.newPlot('breastCancerHistogram', data, layout)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  buildPlot()
+})
--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,7 +1,7 @@
 const baseUrl = 'https://health.data.ny.gov/resource/gnzp-ekau.json'
 
-const fetchCancerPatients = async () => {
-  const res = await fetch(baseUrl + `?$where=UPPER(ccs_diagnosis_description) like '%25CANCER%25'&$limit=1000`, {
+const fetchAllCancer = async () => {
+  const res = await fetch(baseUrl + `?$where=UPPER(ccs_diagnosis_description) like '%25CANCER%25'&$limit=10000`, {
     method: 'GET',
     headers: {
       'X-App-Token': `52qt6guHRHKFEmYLSbmAVVwEc`
@@ -13,7 +13,7 @@ const fetchCancerPatients = async () => {
 }
 
 const fetchBreastCancer = async () => {
-  const res = await fetch(baseUrl + `?$limit=2000&ccs_diagnosis_description=Cancer of breast&gender=F`, {
+  const res = await fetch(baseUrl + `?$limit=10000&ccs_diagnosis_description=Cancer of breast&gender=F`, {
     method: 'GET',
     headers: {
       'X-App-Token': `52qt6guHRHKFEmYLSbmAVVwEc`,
